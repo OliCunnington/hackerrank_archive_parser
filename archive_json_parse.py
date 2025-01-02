66950144e5beacda8459b914f25e0b337f4af91b
Oli <ofcunnington@hotmail.com>
2025-01-02 20:29:19 +0100

    basic plan

--- archive_json_parse.py
+++ archive_json_parse.py
@@ -1,5 +1,16 @@
 import json
 
+'''
+take archive - arg
+get uniq list string names / languages
+replace " " and "/" with "_"
+create dir for ea
+take challenge name
+replace " " and "/" with "_"
+create/write code file
+done?
+'''
+
 if __name__ == "__main__":
     with open("ofcunnington_data.json") as f:
         parsed = json.load(f)
