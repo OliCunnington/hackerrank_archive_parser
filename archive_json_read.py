7a9a3514df41e710496cfddecd8c0f1ef1000f25
Oli <ofcunnington@hotmail.com>
2025-01-05 17:02:12 +0100

    initial dir creations

--- archive_json_read.py
+++ archive_json_read.py
@@ -1,19 +1,23 @@
 import json
+import os
+
 
 if __name__ == "__main__":
     type_dict = {}
-    with open("./ofcunnington_data_format.json") as f:
+    with open("./hackerrank_archive_parser/ofcunnington_data_format.json") as f:
         parsed = json.load(f)
         for k in parsed["submissions"]:
             lang = k["language"].replace("[", "").replace("]", "").replace("\\", "").replace(" ", "_").replace(",", "").replace("\"", "")
             if lang not in type_dict.keys():
-                type_dict[lang] = [k["challenge"].replace(" ", "_")]
+                type_dict[lang] = [k["challenge"].lower().replace(" ", "_")]
             else:
-                
-                type_dict[lang] += [k["challenge"].replace(" ", "_")]
+                type_dict[lang] += [k["challenge"].lower().replace(" ", "_")]
         print(json.dumps(type_dict, indent=4))
         for d in type_dict.keys():
             print(d, ": ", len(type_dict[d]))
+            path = "./hackerrank/" + d
+            if not os.path.exists(path):
+                os.makedirs(path)
         # for k in parsed["submissions"]:
             # type_dict[k["language"]].append(k["code"])
         # print(json.dumps(type_dict, indent = 4))
